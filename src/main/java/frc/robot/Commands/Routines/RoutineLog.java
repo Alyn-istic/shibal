fbf793b8815547b5142ba3cd85425cb943488a0c
Wilson <wilsonyu657@gmail.com>
2024-02-11 16:08:00 -0500

    Added routine log command

---
+++ src/main/java/frc/robot/Commands/Routines/RoutineLog.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands.Routines;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class RoutineLog extends InstantCommand {
+  private String routine;
+  public RoutineLog(String routine) {this.routine = routine;}
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {SmartDashboard.putString("Routine Log", routine);}
+}
