c06c45262d817593fdbcf43a932d1bde735fe094
Wilson <wilsonyu657@gmail.com>
2024-02-15 17:39:47 -0500

    Updated comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -49,9 +49,9 @@ public class Constants {
         public static final int raiseLimitSwitchChannel = 1;
         public static final int dropLimitSwitchChannel = 0;
 
-        public static final double kP = 0.08; // P value for the raise PID command
-        public static final double kI = 0; // I value for the raise PID command
-        public static final double kD = 0; // D value for the raise PID command
+        public static final double kP = 0.08; // P value for the arm PID command
+        public static final double kI = 0; // I value for the arm PID command
+        public static final double kD = 0; // D value for the arm PID command
         public static final double tolerance = 0;
 
         public static final double shootAngle = 100; // The angle of the arm in shooting position
