921fdf7d2a07e2ea7e9277a40a1a739fa95ddc8a
EomLions <eomlions@gmail.com>
2024-03-07 13:08:22 -0500

    Continued testing

--- src/main/java/frc/robot/Commands/MainAutos/Timed/ScoreInAmpTimed/ScoreInAmpTimed0.java
+++ src/main/java/frc/robot/Commands/MainAutos/Timed/ScoreInAmpTimed/ScoreInAmpTimed0.java
@@ -9,11 +9,14 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Commands.Arm.Autos.ArmIntake;
 import frc.robot.Commands.Arm.Autos.ArmShoot;
 import frc.robot.Commands.Arm.Autos.ArmZero;
+import frc.robot.Commands.Drivetrain.TankDriveAutoCmd;
 import frc.robot.Commands.Drivetrain.TankDriveCmd;
 import frc.robot.Commands.Drivetrain.Autos.Timed.LeaveAmpTimed;
 import frc.robot.Commands.Drivetrain.Autos.Timed.TurnToAmpTimed;
 import frc.robot.Commands.Drivetrain.Autos.Timed.MoveToAmpTimed.MoveToAmpTimedForward;
 import frc.robot.Commands.IntakeShooter.Autos.DownShootAmpTimed;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.AutonomousConstants.MoveToAmpTimedConstants;
 import frc.robot.Subsystems.ArmSubsystem;
 //import frc.robot.Subsystems.ArmSubsystem;
 import frc.robot.Subsystems.DrivetrainSubsystem;
@@ -51,7 +54,20 @@ public class ScoreInAmpTimed0 extends SequentialCommandGroup {
 
       new TurnToAmpTimed(driveSub, 1),
       new ArmIntake(armSub, () -> armSub.getController().atSetpoint()),
-      new LeaveAmpTimed(driveSub).raceWith(new DownShootAmpTimed(intakeShooterSub))
+      new LeaveAmpTimed(driveSub).raceWith(new DownShootAmpTimed(intakeShooterSub)),
+
+      new WaitCommand(1),
+
+      new ArmShoot(armSub, () -> armSub.getController().atSetpoint()),
+      new TankDriveCmd(driveSub, () -> 0.3, () -> 0.3).withTimeout(1),
+      new WaitCommand(1),
+      new TankDriveAutoCmd(driveSub,
+        () -> -MoveToAmpTimedConstants.SPEED_TURN_TO_AMP,
+        () -> MoveToAmpTimedConstants.SPEED_TURN_TO_AMP,
+        ()-> 2
+      ),
+      new TankDriveCmd(driveSub, () -> 0.3, () -> 0.3).withTimeout(1),
+      new DownShootAmpTimed(intakeShooterSub)
       
     );
   }
